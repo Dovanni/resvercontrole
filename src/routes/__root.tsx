9502c9d164cca6d0fe2fac269542c0b88d33f590
Antonio Roberto Dovanni Rodrigues <166870450+Dovanni@users.noreply.github.com>
2026-09-15 15:26:13 -0300

    chore(analytics): prepare TanStack SPA pageview emitter

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -121,6 +121,27 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   );
 }
 
+function SpaAnalyticsEmitter() {
+  const router = useRouter();
+
+  useEffect(() => {
+    return router.subscribe("onRendered", () => {
+      const analyticsWindow = window as typeof window & {
+        dataLayer?: Array<Record<string, unknown>>;
+      };
+
+      analyticsWindow.dataLayer = analyticsWindow.dataLayer || [];
+      analyticsWindow.dataLayer.push({
+        event: "vejamais_spa_page_view",
+        page_location: window.location.href,
+        page_title: document.title,
+      });
+    });
+  }, [router]);
+
+  return null;
+}
+
 function RootComponent() {
   const { queryClient } = Route.useRouteContext();
   return (
@@ -128,6 +149,7 @@ function RootComponent() {
       <AuthProvider>
         <SecurityProvider>
           <TooltipProvider>
+            <SpaAnalyticsEmitter />
             <Outlet />
             <Toaster richColors position="top-right" />
           </TooltipProvider>
